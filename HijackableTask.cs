9db91a8afbae9a996fbdeec3ea188320f09e0954
latortuga71 <latortuga71@aol.com>
2022-02-14 11:09:17 -0500

    Add project files.

---
+++ HijackableTask.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.Win32;
+
+
+namespace SharpComHijack
+{
+    public sealed class HijackableTask : SharpSploitResult
+    {
+        public string Name { get; set; }
+        public string Guid { get; set; }
+        public string Trigger { get; set; }
+        public string Path { get; set; }
+        public string RegistryKey { get; set; }
+
+
+        protected internal override IList<SharpSploitResultProperty> ResultProperties => new List<SharpSploitResultProperty>
+        {
+            new SharpSploitResultProperty{Name = nameof(Name),Value = Name},
+            new SharpSploitResultProperty{Name = nameof(Guid),Value = Guid},
+            //new SharpSploitResultProperty{Name = nameof(Trigger),Value = Trigger},
+            new SharpSploitResultProperty{Name = nameof(Path),Value = Path},
+            new SharpSploitResultProperty{Name = nameof(RegistryKey),Value = RegistryKey},
+
+        };
+
+        public bool CheckIfHijackable() 
+        {
+            string registryPath = $"SOFTWARE\\Classes\\CLSID\\{Guid}\\InProcServer32";
+            RegistryKey comObjKey = Registry.CurrentUser.OpenSubKey(registryPath);
+            if (comObjKey == null)
+            {
+                // get key from hklm to get dll path.
+                RegistryKey LMcomObjKey = Registry.LocalMachine.OpenSubKey(registryPath);
+                if (LMcomObjKey == null)
+                    return false;
+                if (LMcomObjKey.GetValue("") == null)
+                    return false;
+                // setting dll path
+                Path = LMcomObjKey.GetValue("").ToString().Trim();
+                RegistryKey = registryPath;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Microsoft.Win32.TaskScheduler;
+
+namespace SharpComHijack
+{
+    class Program
+    {
+        public static List<Task> availableTasks = new List<Task>();
+
+        public static void EnumAllTasks()
+        {
+            TaskFolder fld = TaskService.Instance.RootFolder;
+            foreach (Task task in fld.Tasks)
+                if (ActOnTask(task) == true)
+                    availableTasks.Add(task);
+            foreach (TaskFolder sfld in fld.SubFolders)
+                EnumFolderTasks(sfld);
+        }
+
+        public static bool ActOnTask(Task t) {
+            if(CheckIfComHandler(t) && CheckForTriggers(t))
+                return true;
+            return false;
+        }
+
+        public static bool CheckForTriggers(Task t)
+        {
+            foreach (Trigger tr in t.Definition.Triggers)
+            {
+                if (tr.TriggerType == TaskTriggerType.Logon)
+                    return true;
+            }
+            return false;
+        }
+        public static bool CheckIfComHandler (Task t)
+        {
+            foreach (Microsoft.Win32.TaskScheduler.Action a in t.Definition.Actions)
+            {
+                if (a.ActionType == Microsoft.Win32.TaskScheduler.TaskActionType.ComHandler)
+                    return true;
+            }
+            return false;
+        }
+        public static void EnumFolderTasks(TaskFolder fld)
+        {
+            foreach (Task task in fld.Tasks)
+                if (ActOnTask(task))
+                    availableTasks.Add(task);
+            foreach (TaskFolder sfld in fld.SubFolders)
+                EnumFolderTasks(sfld);
+        }
+
+        public static string GetPotentialTargets()
+        {
+            EnumAllTasks();
+            var targets = new SharpSploitResultList<HijackableTask>();
+            foreach (Task t in availableTasks)
+            {
+                HijackableTask hijack = new HijackableTask();
+                hijack.Name = t.Name.Trim();
+                string[] guidArray = t.Definition.XmlText.Split(new string[] { "<ClassId>" }, System.StringSplitOptions.None);
+                string[] guidArrayBetter = guidArray[1].Split(new string[] { "</ClassId>" }, System.StringSplitOptions.None);
+                hijack.Guid = guidArrayBetter[0];
+                string triggers = "";
+                foreach (Trigger tr in t.Definition.Triggers)
+                {
+                    triggers += tr.ToString().Trim();
+                }
+                hijack.Trigger = triggers;
+                if (hijack.CheckIfHijackable())
+                {
+                    targets.Add(hijack);
+                }
+            }
+            return targets.ToString();
+        }
+        public static void Main(string[] args)
+        {
+            string targets = GetPotentialTargets();
+            System.Console.WriteLine(targets);
+        }
+    }
+}
